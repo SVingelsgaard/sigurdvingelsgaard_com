ca46b5b67ef9cecbe2dfff429cfaf73302c0f88a
SVingelsgaard <sigurd@vingelsgaard.no>
2022-06-12 22:33:36 +0200

    x

--- src/settings.py
+++ src/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!l957a*bli(=vuky))g+c3ch*9!b*44qbx*6yfw)r%d^#7#o!8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['sigurd-website.herokuapp.com','127.0.0.1']
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'home/static'),
     ]
