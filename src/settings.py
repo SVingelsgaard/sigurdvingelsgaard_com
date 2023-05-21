47d42cd74716b4056d67f274621336f61e73074c
SVingelsgaard <sigurd@vingelsgaard.no>
2023-05-21 13:07:41 +0200

    allowed hosts

--- src/settings.py
+++ src/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-!l957a*bli(=vuky))g+c3ch*9!b*44qbx*6yfw)r%d^#7#o!8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sigurd-website.herokuapp.com', 'web-production-ab30.up.railway.app']
+ALLOWED_HOSTS = ['sigurdvingelsgaard.com']
 
 
 # Application definition
